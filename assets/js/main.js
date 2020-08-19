8a3c2d7da9295b415aac75f45ee1e0faf31abe2b
AJIL-PAUL <ajilpaul.268@gmail.com>
2020-08-20 00:41:24 +0530

    modified animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,31 +50,3 @@ jQuery(document).ready(function ($) {
     $("body,html").animate({ scrollTop: target.offset().top }, 600);
   }
 });
-$(function () {
-  $(".flip").flip({
-    trigger: "hover",
-    axis: "y",
-  });
-});
-//RED BORDER ON INVALID INPUT
-document.getElementById("contact-me").addEventListener(
-  "invalid",
-  function (event) {
-    this.classList.add("check");
-  },
-  true
-);
-
-// TEXT AREA AUTO EXPAND
-var textarea = document.querySelector("textarea.autoexpand");
-
-textarea.addEventListener("keydown", autosize);
-
-function autosize() {
-  var el = this;
-  setTimeout(function () {
-    el.style.cssText = "height:auto; padding: 1.4rem .2rem .5rem";
-
-    el.style.cssText = "height:" + el.scrollHeight + "px";
-  }, 0);
-}
